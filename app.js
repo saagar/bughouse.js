ce01de16ed1b439988b8952790fd50978cfefd13
Martin Camacho <camacho@college.harvard.edu>
2013-01-11 18:14:12 -0600

    test

--- app.js
+++ app.js
@@ -29,7 +29,10 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+app.get('/', function(req, res){
+  res.render('index', { title: 'Express' });
+});
+
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
