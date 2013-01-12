74e73f3e7885d28f1e42a604143fa749940f7411
Martin Camacho <camacho@college.harvard.edu>
2013-01-11 18:34:34 -0600

    gitignore

--- app.js
+++ app.js
@@ -35,6 +35,8 @@ app.get('/', function(req, res){
 
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+var io = require('socket.io').listen(server);
--- public/javascripts/bughouse.js
+++ public/javascripts/bughouse.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-	console.log("Starting up.");
+	console.log("Bughouse starting up.");
+
+	// intialize board
+	var paper = Raphael("board_container", 600, 600);
+	var circle = paper.circle(50, 40, 10);
+
 });
